8c259658bc5f842c16e7fdc1eb2783e769ecc11e
skunapuli03 <100485552+skunapuli03@users.noreply.github.com>
2025-03-20 11:39:27 -0400

    made edits to server.js so it can post to the endpoint based on the link given by render.com

--- src/api/server.js
+++ src/api/server.js
@@ -5,12 +5,12 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors({
-  origin: 'https://ugood.vercel.app', // Adjust this to your frontend URL
+  origin: 'https://ugood.vercel.app', 
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 }));
 app.use(bodyParser.json());
-app.post('/generate-reflections', handler);
+app.post('https://ugood-3osi.onrender.com/generate-reflections', handler);
 
 const PORT = process.env.PORT ||9999;
 app.listen(PORT, () => {
